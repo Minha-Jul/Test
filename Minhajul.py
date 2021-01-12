f32772a2ed75539fde7cb2a156a15bcdcde88ffd
Minha-Jul <77315175+Minha-Jul@users.noreply.github.com>
2021-01-12 17:27:25 +0600

    Create Minhajul.py

---
+++ Minhajul.py
@@ -0,0 +1,24 @@
+## This Is A Testing Tool
+
+
+# Commands Tool
+
+* pkg update
+
+* pkg upgrade
+
+* pkg install python
+
+* pkg install python2
+
+* pip2 install requests mechanize
+
+* pkg install git
+
+* git clone https://github.com/Minhajul-Nadim/Test
+
+* cd Test
+
+* python2 Test.py
+
+## Thanks For Using My Commands
